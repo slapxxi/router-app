8632ac5b557ea1f18778be3d8e2a47df2c06263c
Slava Pavlutin <sl.pavlutin@gmail.com>
2025-08-06 09:51:39 +0300

    Install prettier

--- app/routes/home.route.tsx
+++ app/routes/home.route.tsx
@@ -1,32 +1,24 @@
 import { useState } from 'react';
-import { Form, useNavigate } from 'react-router';
+import { Form } from 'react-router';
 import type { Route } from './+types/home.route';
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: 'New React Router App' },
-    { name: 'description', content: 'Welcome to React Router!' },
-  ];
+  return [{ title: 'New React Router App' }, { name: 'description', content: 'Welcome to React Router!' }];
 }
 
 export default function HomeRoute({}: Route.ComponentProps) {
   const [count, setCount] = useState(0);
-  const nav = useNavigate();
 
   console.log('render');
 
   async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    setCount((c) => c + 1);
-    await nav('/about');
-    console.log('after nav', count);
-    setCount((c) => c + 1);
   }
 
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <div className="mx-auto flex w-full max-w-xl flex-col rounded bg-neutral-50 p-4 shadow-lg/10">
-        <h2 className="mb-4 text-lg font-semibold">Login</h2>
+    <div className="flex min-h-screen items-center justify-center bg-pink-200/10">
+      <div className="mx-auto flex w-full max-w-xl flex-col rounded bg-white p-4 shadow-lg/10">
+        <h2 className="mb-4 text-lg font-semibold">Login {count}</h2>
 
         <div className="flex flex-col py-2">
           <input
@@ -40,18 +32,8 @@ export default function HomeRoute({}: Route.ComponentProps) {
         </div>
 
         <Form id="my-form" className="flex flex-col gap-2" onSubmit={handleSubmit} method="POST">
-          <input
-            type="text"
-            name="firstName"
-            className="rounded border bg-white p-2"
-            placeholder="First Name"
-          />
-          <input
-            type="text"
-            name="secondName"
-            className="rounded border bg-white p-2"
-            placeholder="Second Name"
-          />
+          <input type="text" name="firstName" className="rounded border bg-white p-2" placeholder="First Name" />
+          <input type="text" name="secondName" className="rounded border bg-white p-2" placeholder="Second Name" />
 
           <div className="flex gap-2">
             <label className="text-sm" htmlFor={'red'}>
@@ -68,9 +50,7 @@ export default function HomeRoute({}: Route.ComponentProps) {
             <input id="green" type="checkbox" name="color" value="green" />
           </div>
 
-          <button className="rounded bg-blue-500 p-2 text-white hover:bg-blue-600 active:translate-y-px">
-            Submit
-          </button>
+          <button className="rounded bg-blue-500 p-2 text-white hover:bg-blue-600 active:translate-y-px">Submit</button>
         </Form>
       </div>
     </div>
---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config} */
+const config = {
+  printWidth: 120,
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: "es5",
+  arrowParens: "always",
+};
+
+export default config;
