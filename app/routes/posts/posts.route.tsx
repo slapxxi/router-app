1201f6dd0a15787e14bc2eaff96f457a95e5430d
Slava Pavlutin <sl.pavlutin@gmail.com>
2025-08-17 09:42:19 +0300

    Link to home page in posts

--- app/routes/posts/posts.route.tsx
+++ app/routes/posts/posts.route.tsx
@@ -1,10 +1,21 @@
-import { Outlet } from 'react-router';
+import { href, Link, Outlet } from 'react-router';
 import type { Route } from './+types/posts.route';
 
 export default function PostsLayoutRoute({}: Route.ComponentProps) {
   return (
     <div className="p-2">
+      <header>
+        <nav>
+          <ul className="flex gap-2">
+            <li>
+              <Link to={href('/')}>Home</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
       <h1 className="text-xl font-bold">Posts</h1>
+
       <Outlet />
     </div>
   );
